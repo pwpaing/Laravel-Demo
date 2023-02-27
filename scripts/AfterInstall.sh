ad9ffe40ddbe85290541969134b1432d61997e26
phyowaipaing <34337017+pwpaing@users.noreply.github.com>
2023-02-28 01:47:44 +0630

    Update AfterInstall.sh

--- scripts/AfterInstall.sh
+++ scripts/AfterInstall.sh
@@ -2,10 +2,11 @@
 
 cd /var/www/laravel
 #aws s3 cp s3://aws-codedeploy-deployments/api-env/.env .env
-rm composer.lock
-sudo /usr/bin/composer install 
-sudo php artisan config:clear
+#rm composer.lock
+#sudo /usr/bin/composer install 
+#sudo php artisan config:clear
 
-sudo chown -R www-data:www-data /var/www/laravel
+#sudo chown -R www-data:www-data /var/www/laravel
+sudo chown -R apache:apache /var/www/laravel
 sudo chmod -R 0777 /var/www/laravel/storage
 sudo chmod -R 0777 /var/www/laravel/bootstrap/cache
